f48e4159196570ff8db2f78c5ad86f02bf9ae2e5
Mraz Tevin <millatevin@gmail.com>
2022-01-26 09:05:31 +0300

    enabled scheduling and created new class

--- src/main/java/com/trackerapp/covidtracker/services/CoronaDataService.java
+++ src/main/java/com/trackerapp/covidtracker/services/CoronaDataService.java
@@ -11,6 +11,7 @@ import javax.annotation.PostConstruct;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,6 +19,7 @@ public class CoronaDataService {
     private static String DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
     @PostConstruct
+    @Scheduled(cron = "* * * * * *")
     public void fetchVirusData() throws IOException, InterruptedException {
                 HttpClient client = HttpClient.newHttpClient();
                  HttpRequest request = HttpRequest.newBuilder()
