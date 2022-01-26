b16e7ac5147d5de6bf953a54c9146a353a641568
Mraz Tevin <millatevin@gmail.com>
2022-01-26 09:29:27 +0300

    add getter and setter methods

--- src/main/java/com/trackerapp/covidtracker/models/LocationStats.java
+++ src/main/java/com/trackerapp/covidtracker/models/LocationStats.java
@@ -4,5 +4,26 @@ public class LocationStats {
     private String state;
     private String country;
     private int latestTotalCases;
-    
+
+    public String getState() {
+        return state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public int getTotalCases() {
+        return latestTotalCases;
+    }
+    public void setState(String state) {
+        this.state = state;
+    }
+    public void setCountry(String country) {
+        this.country = country;
+    }
+    public void setLatestTotalCases(int latestTotalCases) {
+        this.latestTotalCases = latestTotalCases;
+    }
+
 }
