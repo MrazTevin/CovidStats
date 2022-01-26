8770d9d3862a19724bb6a866d470aacfdae67b61
Mraz Tevin <millatevin@gmail.com>
2022-01-26 18:26:30 +0300

    include parameters in method and add thymeleaf syntax

--- src/main/java/com/trackerapp/covidtracker/controllers/HomeController.java
+++ src/main/java/com/trackerapp/covidtracker/controllers/HomeController.java
@@ -1,13 +1,15 @@
 package com.trackerapp.covidtracker.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("testname", "TEST");
         return "home";
     }
 }
