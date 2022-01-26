b26bdc9e1a18c3b28e84249dcc980f8dd7b4447e
Mraz Tevin <millatevin@gmail.com>
2022-01-26 09:43:09 +0300

    call the setter methods

--- src/main/java/com/trackerapp/covidtracker/services/CoronaDataService.java
+++ src/main/java/com/trackerapp/covidtracker/services/CoronaDataService.java
@@ -6,8 +6,12 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
+
+import com.trackerapp.covidtracker.models.LocationStats;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
@@ -18,9 +22,11 @@ import org.springframework.stereotype.Service;
 public class CoronaDataService {
     private static String DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
+    private List<LocationStats> allStats = new ArrayList();
     @PostConstruct
     @Scheduled(cron = "* * * * * *")
     public void fetchVirusData() throws IOException, InterruptedException {
+                List<LocationStats> newStats = new ArrayList<>();
                 HttpClient client = HttpClient.newHttpClient();
                  HttpRequest request = HttpRequest.newBuilder()
                         .uri(URI.create(DATA_URL))
@@ -30,8 +36,10 @@ public class CoronaDataService {
                 StringReader csvBodyReader = new StringReader(httpResponse.body());
                 Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
                 for (CSVRecord record : records) {
-                     String country = record.get("Country/Region");
-                     System.out.println(country);
+                    LocationStats locationStat = new LocationStats();
+                    locationStat.setState(record.get("Province/State"));
+                    locationStat.setCountry(record.get("Region/Country"));
+                    locationStat.setLatestTotalCases(Integer.parseInt(record.get(record.size() - 1)));
                 }
     }
 }
